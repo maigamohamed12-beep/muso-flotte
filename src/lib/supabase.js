18c0ce03dd9eaf517ec99e8627d0ad6a4213d655
maigamohamed12-beep <maigamohamed12@gmail.com>
2026-05-11 15:39:45 +0000

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -3,8 +3,4 @@ import { createClient } from '@supabase/supabase-js'
 const url = import.meta.env.VITE_SUPABASE_URL
 const key = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-if (!url || !key) {
-  console.error('Variables Supabase manquantes !')
-}
-
 export const supabase = createClient(url, key)
